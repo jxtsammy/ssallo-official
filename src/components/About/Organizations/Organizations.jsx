4c69ecbe6f9e492f1607020d9957456274251a1f
Your Name <you@example.com>
2026-05-26 00:42:53 +0000

    ssallo

--- src/components/About/Organizations/Organizations.jsx
+++ src/components/About/Organizations/Organizations.jsx
@@ -4,7 +4,7 @@ import './Organizations.css';
 // Leave placeholders for your actual background images
 import EnactusBg from '../../../assets/organizations/Enactus.jpg';
 import AgriCycleBg from '../../../assets/organizations/Agricycle.jpg';
-import SunawashBg from '../../../assets/organizations/IMG_0233.jpg';
+import SunawashBg from '../../../assets/organizations/Wash.jpg';
 import NextMakers from '../../../assets/organizations/NextMakers.jpg';
 
 const Organizations = () => {
